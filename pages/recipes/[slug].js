f36beb3cd85cd78652b9753b1862588e3708cb32
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2022-08-31 14:55:59 +0200

    fallbackPages

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -20,7 +20,7 @@ export const getStaticPaths = async () => {
 
     return {
         paths,
-        fallback: false
+        fallback: true
     }
 }
 
@@ -39,6 +39,7 @@ export const getStaticProps = async ({params}) => {
 
 export default function RecipeDetails({recipe}) {
     // console.log(recipe)
+    if (!recipe) return <div> loading...</div>
     const {featuredImage, title, cookingTime, ingredients, method} = recipe.fields
 
     return (
